35ad289efb32902a8e8d45b228c96bd945e82718
JhonMercedes <jhonzeras93@gmail.com>
2021-12-01 12:24:10 -0300

    up

--- .history/cap1/js/ex1_11b_20211201113911.js
+++
@@ -1,7 +0,0 @@
-
-    var valorConta = Number(prompt("Informe o valor total da conta: "));
-    var numPessoas = Number(prompt("Informe a quantidade de pessoas na mesa: "));
-
-    valorPorCliente = valorConta / numPessoas;
-
-    alert("Valor por cliente: " + valorPorCliente);
--- .history/cap1/js/ex1_11c_20211201121459.js
+++
@@ -1,8 +0,0 @@
- var precoProduto = Number(prompt("Informe o valor do produto: "));
-
-    desconto = precoProduto * 10/100;
-    precoFinal = precoProduto - desconto;
-    parcela = precoProduto / 3
-
-    alert("Preço Produto R$: " + precoProduto.toFixed(2) + "\nA vista R$ "
-         + precoFinal.toFixed(2) + "\nOu em 3x R$ " + parcela.toFixed(2));
