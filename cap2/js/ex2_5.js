3b65171a3c38433d29d35d13a8bcef4795cb3929
JhonMercedes <jhonzeras93@gmail.com>
2021-12-06 22:42:49 -0300

    correção js

--- cap2/js/ex2_5.js
+++ cap2/js/ex2_5.js
@@ -1,20 +1,22 @@
-function mostraMedicamento() {
-    var medicamento = document.getElementById("inMedicamento");
-    document.getElementById("outMedicamento").textContent = medicamento;
 
-}
-function calcularPreco() {
-    
+function calcularPromocao() {
+
+    var inMedicamento = document.getElementById("inMedicamento");
     var inPreco = document.getElementById("inPreco");
+    var outMedicamento = document.getElementById("outMedicamento");
     var outValor = document.getElementById("outValor");
 
-    var preco = Number(inPreco.value);
     var medicamento = inMedicamento.value;
+    var preco = Number(inPreco.value);
+    
 
-    var valor = (preco*2);
+    //var valor = preco * 2; 
+    valor = Math.floor(preco)*2;
 
+    outMedicamento.textContent = medicamento;
     outValor.textContent = "Leve 2 e page R$ " + valor;
    
-    var btCalcular = document.getElementById("btCalcular");
-    btCalcular.addEventListener("click", calcularPreco);
 }
+
+var btCalcular = document.getElementById("btCalcular");
+    btCalcular.addEventListener("click", calcularPromocao);
